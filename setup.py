715ad45c1ecbaed3f12df960bbd4720f9c4ca2ac
Sadra Sadraddini <sadra@bu.edu>
2019-10-28 20:02:58 -0400

    create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='pypolycontain',
+    version='1.0',
+    packages=['pypolycontain',],
+    license='MIT',
+    long_description=open('README.txt').read(),
+)
